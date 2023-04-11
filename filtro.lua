48381c2c69ad51d846543c16e12677cd2d9d01cd
Σάμιος Γεώργιος <92584026+p20sami@users.noreply.github.com>
2023-04-11 14:23:21 +0300

    more fixes on filter

--- filtro.lua
+++ filtro.lua
@@ -4,7 +4,7 @@ function Image(img)
         local doc = pandoc.read(f:read('*a'))
         f:close()
         local caption = pandoc.utils.stringify(doc.meta.caption) or "Caption has not been set"
-        local person = pandoc.utils.stringify(doc.meta.person) or "Student has not been set"
+        local student = pandoc.utils.stringify(doc.meta.student) or "Student has not been set"
         local AM = pandoc.utils.stringify(doc.meta.AM) or "AM has not been set"
         local title = ">" .. " Caption:" .. caption .. "  \n>" .. " Student:" .. student .. "  \n>" .. " AM:" .. AM
         return pandoc.RawInline('markdown',title)
